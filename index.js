adf8565b4d3ec405fe45a4c0128597965a461b99
Daniel Stainback <torann@gmail.com>
2015-06-25 12:16:18 -0400

    Add merge-stream

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var gulp          = require('gulp'),
     elixir        = require('laravel-elixir'),
+    merge         = require('merge-stream'),
     _             = require('underscore'),
     wrap          = require('gulp-wrap'),
     concat        = require('gulp-concat'),
@@ -19,37 +20,47 @@ elixir.extend('templates', function (src, options) {
         search: '/**/*.hbs'
     }, options);
 
-    src = "./" + utilities.buildGulpSrc(src + options.search, options.srcDir);
+    config.saveTask('templates', {
+        src: "./" + utilities.buildGulpSrc(src + options.search, options.srcDir),
+        options: options
+    });
 
     gulp.task('templates', function () {
 
+        var dataSet = config.collections['templates'];
+
         var onError = function (e) {
-            new notifications().error(e, 'Handlebars Template Compilation Failed!');
+            new notification().error(e, 'Handlebars Template Compilation Failed!');
             this.emit('end');
         };
 
-        return gulp.src(src)
-            .on('error', onError)
-            .pipe(handlebars())
-            // Wrap each template function in a call to Handlebars.template
-            .pipe(wrap('Handlebars.template(<%= contents %>)'))
-            // Declare template functions as properties and sub-properties of exports
-            .pipe(declare({
-                root       : 'exports',
-                noRedeclare: true, // Avoid duplicate declarations
-                processName: function (filePath) {
-                    return declare.processNameByPath(filePath.replace(options.srcDir + '/', ''));
-                }
-            }))
-            // Concatenate down to a single file
-            .pipe(concat(options.outputFile))
-            // Add the Handlebars module in the final output
-            .pipe(wrap('var Handlebars = require("handlebars");\n <%= contents %>'))
-            .pipe(gulp.dest(options.outputDir))
-            .pipe(new notification().message('Handlebars Template Compiled'));
+        return merge.apply(this, dataSet.map(function(data) {
+
+            var options = data.options;
+
+            return gulp.src(data.src)
+                .on('error', onError)
+                .pipe(handlebars())
+                // Wrap each template function in a call to Handlebars.template
+                .pipe(wrap('Handlebars.template(<%= contents %>)'))
+                // Declare template functions as properties and sub-properties of exports
+                .pipe(declare({
+                    root       : 'exports',
+                    noRedeclare: true, // Avoid duplicate declarations
+                    processName: function (filePath) {
+                        return declare.processNameByPath(filePath.replace(options.srcDir + '/', ''));
+                    }
+                }))
+                // Concatenate down to a single file
+                .pipe(concat(options.outputFile))
+                // Add the Handlebars module in the final output
+                .pipe(wrap('var Handlebars = require("handlebars");\n <%= contents %>'))
+                .pipe(gulp.dest(options.outputDir))
+                .pipe(new notification().message('Handlebars Template Compiled'));
+        }));
     });
 
-    this.registerWatcher('templates', src, 'default');
-
-    return this.queueTask('templates');
+    return config
+        .registerWatcher('templates', src)
+        .queueTask('templates');
 });
